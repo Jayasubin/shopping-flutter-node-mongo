02494edf98bcf2ac3f4c9de2faea55d53c43e80d
Jayasubin J <subin@crestronics.com>
2022-07-18 11:25:22 +0530

    added flutter project

---
+++ shop_flutter/README.md
@@ -0,0 +1,10 @@
+# shop_flutter
+
+Shopping app
+    -Seller
+    -Buyer
+    -Cart
+
+A node.js api
+
+A mongo Database
---
+++ shop_flutter/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Nothing'),
+      ),
+      body: const Center(
+        child: Text('Nothing'),
+      ),
+    );
+  }
+}
